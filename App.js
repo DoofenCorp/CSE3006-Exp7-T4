b4c201fc083d7937eff43db6007b788e015d1ef2
DoofenCorp <48542507+DoofenCorp@users.noreply.github.com>
2022-12-06 00:58:52 +0530

    Init

---
+++ App.js
@@ -0,0 +1,36 @@
+const express = require("express");
+var app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.set("viewengine", "ejs");
+app.use(logger)
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  setInterval(function() {
+    var date = Date()
+    socket.send(date)
+  }, 1000)
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  })
+});
+
+function logger(req, res, next) {
+  console.log(req.method + " - " + req.ip)
+  next()
+}
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
+
+
+server.listen(3000, () => {
+  console.log('Listening on PORT:3000');
+});
---
+++ README.md
@@ -0,0 +1,22 @@
+# CSE3006-Exp7
+
+This is the experiment resource repository. Fork, clone and follow below.
+
+### Built with:
+
+* JavaScript
+* Express.js
+* EJS - Embedded JavaScript Templating
+* [Socket.io](https://socket.io)
+
+## Install the dependencies
+
+`npm install`
+
+## Start the project
+
+`npm start`
+
+### Visit localhost:3000
+
+Deploying sooooon
